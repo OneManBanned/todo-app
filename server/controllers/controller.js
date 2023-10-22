fea34409aeaf35be3a2cbd2fae35e86b06b29316
Brendan Lawless <brendanlawless@hotmail.com>
2023-10-22 21:06:18 +0100

    database connected

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,32 +1,32 @@
+const asyncHandler = require('express-async-handler')
 // @desc    Get todos
 // @route   GET /
 // @access  Private
-const getTodos = (req, res) => {
+const getTodos = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get todo' })
-}
+})
 // @desc    Set todo
 // @route   POST /
 // @access  Private
-const setTodos = (req, res) => {
+const setTodos = asyncHandler(async (req, res) => {
     if (!req.body.text) {
         res.status(400)
         throw new Error('Please add a text field')
     }
     res.status(200).json({ message: 'Set todo' })
-
-}
+})
 // @desc    Update todos
 // @route   Put /:id
 // @access  Private
-const updateTodos = (req, res) => {
+const updateTodos = asyncHandler(async (req, res) => {
     res.status(200).json({ "message": `Update todo ${req.params.id}` })
-}
+})
 // @desc    Delete todos
 // @route   DELETE /:id
 // @access  Private
-const deleteTodos = (req, res) => {
+const deleteTodos = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Delete todo' })
-}
+})
 
 module.exports = {
     getTodos,
