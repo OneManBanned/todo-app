26cfec43162db163f5bc66257a5fb7f46d5bbbd0
Brendan Lawless <brendanlawless@hotmail.com>
2023-12-09 14:09:52 +0000

    Ready for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: '/src/main.jsx'
-    },
-  },
+  plugins: [react()],
   server: {
     origin: "http://localhost:5000",
     proxy: {
       "/app/": "http://localhost:5000", // the address that u serve in the backend 
     },
-  },
-  plugins: [react()]
+  }
 })
--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use('/app/users', require('./routes/userRoutes'))
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')))
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'client', 'index.html')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'client', 'dist', 'index.html')))
 } else {
     app.get('/', (req, res) =>
         res.send('Please set to production'))
