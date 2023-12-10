02363f1d0007181367b331bd21e79ed10d7919ad
Brendan Lawless <brendanlawless@hotmail.com>
2023-12-10 16:37:17 +0000

    Snagging list

--- server/server.js
+++ server/server.js
@@ -21,8 +21,7 @@ if (process.env.NODE_ENV === 'production') {
 
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'client', 'dist', 'index.html')))
 } else {
-    app.get('/', (req, res) =>
-        res.send('Please set to production'))
+    app.use(express.static(path.join(__dirname, '../client/public')))
 }
 
 app.use(errorHandler)
